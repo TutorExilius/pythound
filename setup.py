6a3d8398766c8895f8544a1b7f2eabf20570c1a5
Tutor Exilius <tutorexilius@gmail.com>
2022-02-26 16:52:42 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+install_requires = [
+    'psutil=^5.9.0',
+]
+
+setup(
+    name='pythound',
+    version='0.1.0',
+    description='An async sound library for python.',
+    license="MIT",
+    author='Tutor Exilius',
+    packages=['pythound'],
+    author_email='tutorexilius@gmail.com',
+    install_requires=install_requires,
+)
