c964346a7f8f225fa795f571dbb2cd8f5d5c9961
Tutor Exilius <tutorexilius@gmail.com>
2022-03-03 20:36:48 +0100

    feat: redesign volume and speed handling, add properties class for volume and speed

--- pythound/sound_manager.py
+++ pythound/sound_manager.py
@@ -7,22 +7,28 @@ from typing import List, Optional
 import psutil
 
 
-class Sound:
-    def __init__(self, file_path: Path, sound_volume: Optional[int] = None, sound_speed: int = None) -> None:
-        self.file_path: Path = file_path
-        self.process: Optional[psutil.Process] = None
-        self._loop: Optional[int] = None
-        self._volume = None  # initialize _volume before using probperty 'volume'
-        self.volume: Optional[int] = sound_volume  # volume property validate
-        self.speed: Optional[int] = sound_speed  # volume property validate
+class FFPLAY_Properties:
+    def __init__(
+            self,
+            parent: str,
+            volume: Optional[int] = None,
+            speed: Optional[float] = None,
+    ):
+        self._parent = parent
+        self.volume = volume
+        self.speed = speed
 
     @property
     def volume(self) -> int:
         return self._volume
 
     @volume.setter
-    def volume(self, value: int) -> None:
-        if self._volume != value:
+    def volume(self, value: Optional[int]) -> None:
+        if value is None:
+            if self._parent != "Sound":
+                print("It is not possible to set the player volume to None.")
+                return
+        else:
             if value > 100:
                 print("Volumes above 100 are treated as 100.", flush=True)
                 value = 100
@@ -30,47 +36,109 @@ class Sound:
                 print("Negative volumes are treated as 0.", flush=True)
                 value = 0
 
-            self._volume = value
-            print(f"Set sound volume to {value}.", flush=True)
+        self._volume = value
+        print(f"Set {self._parent} sound volume to {value}.", flush=True)
+
+    @property
+    def speed(self) -> float:
+        return self._speed
+
+    @speed.setter
+    def speed(self, value: Optional[float]) -> None:
+        if value is None:
+            if self._parent != "Sound":
+                print("It is not possible to set the player speed to None.")
+                return
+        else:
+            if value > 100:
+                value = 100
+                print("Speed above 100 is treated as 100.", flush=True)
+            elif value < 0.5:
+                value = 0.5
+                print("Speed less than 0.5 is treated as 0.5", flush=True)
+
+        self._speed = value
+        print(f"Set {self._parent} speed to {value}.", flush=True)
+
+
+class Sound:
+    def __init__(
+            self,
+            app_name: str,
+            sound_file_path: Path,
+            sound_volume: Optional[int] = None,
+            sound_speed: Optional[float] = None,
+    ) -> None:
+        self.app_name = app_name
+        self.sound_file_path: Path = sound_file_path
+        self.process: Optional[psutil.Process] = None
+        self._loop: Optional[int] = None
+
+        if self.app_name == "ffplay":
+            self._properies = FFPLAY_Properties(
+                parent="Sound",
+                volume=sound_volume,
+                speed=sound_speed,
+            )
+
+    @property
+    def volume(self) -> int:
+        return self._properies.volume
+
+    @volume.setter
+    def volume(self, value: int) -> None:
+        self._properies.volume = value
+
+    @property
+    def speed(self) -> float:
+        return self._properies.speed
+
+    @speed.setter
+    def speed(self, value: float) -> None:
+        self._properies.speed = value
 
 
 class Player:
-    def __init__(self, app="ffplay", player_volume: int = 100, player_speed: float = 1.0) -> None:
-        self.app_name: str = app
-        self.app_args: List[str] = []
+    def __init__(
+            self, app_name="ffplay", player_volume: int = 100, player_speed: float = 1.0
+    ) -> None:
+        self.app_name: str = app_name
         self.sound_files: List[Sound] = []  # {sound_id:
-        self._volume = 100  # initialize _volume before using probperty 'volume'
-        self.volume: int = player_volume  # volume property validate
-        self.speed = player_speed
+        self._app_args: List[str] = []
 
         if self.app_name == "ffplay":
-            self.app_args = ["-nodisp", "-autoexit", "-loglevel", "quiet"]
+            self._app_args = ["-nodisp", "-autoexit", "-loglevel", "quiet"]
+            self._properies = FFPLAY_Properties(
+                parent="Player",
+                volume=player_volume or 100,
+                speed=player_speed or 1.0,
+            )
 
     @property
     def volume(self) -> int:
-        return self._volume
+        return self._properies.volume
 
     @volume.setter
     def volume(self, value: int) -> None:
-        if self._volume != value:
-            if value > 100:
-                print("Volumes above 100 are treated as 100.", flush=True)
-                value = 100
-            elif value < 0:
-                print("Negative volumes are treated as 0.", flush=True)
-                value = 0
+        self._properies.volume = value
 
-            self._volume = value
-            print(f"Set player volume to {value}.", flush=True)
+    @property
+    def speed(self) -> float:
+        return self._properies.speed
+
+    @speed.setter
+    def speed(self, value: float) -> None:
+        self._properies.speed = value
 
     def add_sound(
             self,
-            file_path: Path,
+            sound_file_path: Path,
             initial_sound_volume: Optional[int] = None,
             initial_sound_speed: Optional[int] = None,
     ) -> Sound:
         new_sound = Sound(
-            file_path,
+            self.app_name,
+            sound_file_path=sound_file_path,
             sound_volume=initial_sound_volume,
             sound_speed=initial_sound_speed,
         )
@@ -89,7 +157,7 @@ class Player:
             self._reset_process_state(sound)
 
         app_params = [self.app_name]
-        app_params.extend(self.app_args)
+        app_params.extend(self._app_args)
 
         volume = sound.volume if sound.volume else self.volume
         app_params.extend(["-volume", str(volume)])
@@ -100,10 +168,10 @@ class Player:
         speed = sound.speed if sound.speed else self.speed
         app_params.extend(["-af", f"atempo={speed}"])
 
-        app_params.append(str(sound.file_path))
+        app_params.append(str(sound.sound_file_path))
 
         process = subprocess.Popen(app_params)
-        print(f"Play '{sound.file_path}'.", flush=True)
+        print(f"Play '{sound.sound_file_path}'.", flush=True)
 
         ps_process = psutil.Process(pid=process.pid)
         sound.process = ps_process
@@ -118,16 +186,15 @@ class Player:
         else:
             print(
                 "Error in wait_for(): can't wait for a sound, which is in endless loop.",
-                flush=True
+                flush=True,
             )
-            return
 
     def stop(self, sound: Sound) -> None:
         if not sound.process:
             return
 
         self._reset_process_state(sound)
-        print(f"Stop '{sound.file_path}'.", flush=True)
+        print(f"Stop '{sound.sound_file_path}'.", flush=True)
 
     def stop_all(self) -> None:
         for sound in self.sound_files:
@@ -136,14 +203,14 @@ class Player:
     def continue_sound(self, sound: Sound) -> None:
         if not sound.process:
             print(
-                f"Error in continuing '{sound.file_path}', sound is not paused OR is a sound effect, which can't be continued.",
+                f"Error in continuing '{sound.sound_file_path}', sound is not paused OR is a sound effect, which can't be continued.",
                 flush=True,
             )
             return
 
         if sound.process.status() == psutil.STATUS_RUNNING:
             print(
-                f"Error in contining '{sound.file_path}', sound is already playing.",
+                f"Error in contining '{sound.sound_file_path}', sound is already playing.",
                 flush=True,
             )
         elif sound.process.status() in (
@@ -152,24 +219,24 @@ class Player:
                 psutil.STATUS_DISK_SLEEP,
         ):
             sound.process.resume()
-            print(f"Continue '{sound.file_path}'.", flush=True)
+            print(f"Continue '{sound.sound_file_path}'.", flush=True)
         else:
             self._reset_process_state(sound)
 
     def pause_sound(self, sound: Sound) -> None:
         if not sound.process:
             print(
-                f"Error in pausing '{sound.file_path}', sound is not playing OR is a sound effect, which can't paused.",
+                f"Error in pausing '{sound.sound_file_path}', sound is not playing OR is a sound effect, which can't paused.",
                 flush=True,
             )
             return
 
         if sound.process.status() == psutil.STATUS_RUNNING:
             sound.process.suspend()
-            print(f"Pause '{sound.file_path}'.", flush=True)
+            print(f"Pause '{sound.sound_file_path}'.", flush=True)
         elif sound.process.status() == psutil.STATUS_SUSPENDED:
             print(
-                f"Error in pausing '{sound.file_path}', sound is already paused.",
+                f"Error in pausing '{sound.sound_file_path}', sound is already paused.",
                 flush=True,
             )
         else:
